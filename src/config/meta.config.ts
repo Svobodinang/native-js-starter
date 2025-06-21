376fa2d0ad8f891d7b15e6fbc735661b90004c9f
svobodinang@gmail.com <svobodinang@gmail.com>
2025-06-21 14:48:23 +0300

    feat(styles): add colors & sizes

--- src/config/meta.config.ts
+++ src/config/meta.config.ts
@@ -1,5 +1,4 @@
 export default {
-    lang: 'ru',
     title: 'My Awesome Site',
     description: 'Welcome to my awesome site. Built with Vite + Pug.',
     favicon: '/favicon.ico',
--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,41 @@ import { fileURLToPath } from 'node:url';
 import { dirname, resolve } from 'node:path';
 
 import pugPlugin from 'vite-plugin-pug';
+import eslint from 'vite-plugin-eslint';
+import stylelint from 'vite-plugin-stylelint';
 
 import meta from './src/config/meta.config';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+
+
 export default defineConfig({
-    root: 'src/templates',
+    base: "./",
+    root: "./src",
     plugins: [
-        pugPlugin({}, { meta }),
+        pugPlugin({}, { meta, baseUrl: "/pages" }),
+        eslint({
+            cache: false,
+            include: ['src/**/*.ts', 'src/**/*.js'],
+            exclude: ['node_modules', 'dist'],
+            emitWarning: true,
+            emitError: true,
+        }),
+        stylelint({
+            include: ['src/**/*.{css,scss,sass}'],
+            exclude: ['node_modules', 'dist'],
+            emitWarning: true,
+            emitError: true,
+            failOnError: false
+        }),
+        {
+        name: 'remove-crossorigin',
+            transformIndexHtml(html) {
+                return html.replace(/\s+crossorigin/g, '');
+            },
+        },
     ],
     resolve: {
         alias: {
@@ -24,9 +49,9 @@ export default defineConfig({
         emptyOutDir: true,
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'src/templates/index.html'),
-                about: resolve(__dirname, 'src/templates/about.html'),
-            },
-        },
-    },
-});
+                main: "./src/index.html",
+                about: "./src/pages/about/index.html"
+            }
+        }
+    }
+})
